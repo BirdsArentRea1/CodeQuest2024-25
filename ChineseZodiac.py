9094567612b7a8b75e10cdaa6add3cf0064d9ca7
BirdsArentRea1 <146360250+BirdsArentRea1@users.noreply.github.com>
2024-01-17 10:20:38 -0700

    Update ChineseZodiac.py

--- ChineseZodiac.py
+++ ChineseZodiac.py
@@ -13,14 +13,14 @@ for i in range(cases):
         print("Yin", end = " ")
         
     #element
-    elements = ["Wood", "Fire", "Metal", "Water", "Earth"]
+    elements = ["Wood", "Fire", "Earth", "Metal", "Water"]
     element = line - 4
     element = element%10
     element = math.floor(element/2)
     print(elements[element], end = " ")
     
     #animal
-    animals = ["Rat", "Ox", "Tiger", "Rabbit", "Dragon", "Snake" "Horse", "Goat", "Monkey", "Rooster", "Dog", "Pig"]
+    animals = ["Rat", "Ox", "Tiger", "Rabbit", "Dragon", "Snake", "Horse", "Goat", "Monkey", "Rooster", "Dog", "Pig"]
     year = line - 4
     year = year%12
     print(animals[year], end = " ")
