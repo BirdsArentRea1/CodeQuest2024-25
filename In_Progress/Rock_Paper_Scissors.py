f2630415446d11dfc15f62fd1545bfd791534c91
BirdsArentRea1 <146360250+BirdsArentRea1@users.noreply.github.com>
2024-11-12 10:38:13 -0700

    Update Rock_Paper_Scissors.py

--- In_Progress/Rock_Paper_Scissors.py
+++ In_Progress/Rock_Paper_Scissors.py
@@ -1,30 +1,26 @@
-cases = int(input().rstrip())
-R = 0 #Rock
-P = 0 #Paper
-S = 0 #Scissors
-X = 0 #No Winner
+import sys
 
-for i in range(cases):
+cases = int(sys.stdin.readline().rstrip())
+
+for caseNum in range(cases):
     line = input().rstrip()
+    Rock = False
+    Paper = False
+    Scissors = False
+    line = sys.stdin.readline().rstrip()
+    for charactor in line:
+        if charactor == 'R':
+            Rock = True
+        if charactor == 'P':
+            Paper = True
+        if charactor == 'S':
+            Scissors = True
 
-    for j in line:
-        if (j == 'R','P'):
-            P +=1
-        elif (j == 'R','S'):
-            R +=1
-        elif (j == 'P','S'):
-            S +=1
-        elif (j == 'R','P','S'):
-            X +=1
-        elif (j == 'R','R'):
-            X +=1
-
-    if (R > P and R > S and R > X):
-        print("ROCK")
-    elif (P > R and P > S and P > X):
-        print("PAPER")
-    elif (S > R and S > P and S > X):
-        print("SCISSORS")
-    elif (X > R and X > P and X > S):
+    if Rock and Paper and Scissors:
         print("NO WINNER")
-     
+    elif Rock and Paper:
+        print("PAPER")
+    elif Paper and Scissors:
+        print("SCISSORS")
+    elif Rock and Scissors:
+        print("ROCK")    
